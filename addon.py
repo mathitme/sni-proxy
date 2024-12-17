57e3a6d1761271d150b1e13ce18669215241f920
mathitme <bmtaoran@163.com>
2024-12-17 10:24:42 +0800

    openwrt
    now working on docker mitmproxy, with adguardhome user rules added

--- addon.py
+++ addon.py
@@ -2,7 +2,10 @@ from typing import Iterable
 from mitmproxy import tls, http, connection, addonmanager
 from mitmproxy.addons.tlsconfig import TlsConfig
 from mitmproxy.http import HTTPFlow
-import yaml
+import json
+
+# import os
+# print(os.getcwd())
 
 
 def matches_any(domains: Iterable[str], domain: str) -> bool:
@@ -15,12 +18,21 @@ class SNIProxy:
     domains: Iterable[str]
     delicate_domains: Iterable[str]
 
+
     def load(self, _loader: addonmanager.Loader):
-        with open("./domains.yaml") as f:
-            out = yaml.safe_load(f)
+        with open("/home/mitmproxy/.mitmproxy/domains.json") as f:
+            out = json.load(f)
             self.domains = out.get("domains", [])
             self.delicate_domains = out.get("delicate_domains", [])
-            self.sni = out.get("default_sni", "google.com")
+            self.googles = out.get("googles",[])
+            self.sni = out.get("default_sni", "baidu.com")
+
+    # def load(self, _loader: addonmanager.Loader):
+    #     with open("./domains.yaml") as f:
+    #         out = yaml.safe_load(f)
+    #         self.domains = out.get("domains", [])
+    #         self.delicate_domains = out.get("delicate_domains", [])
+    #         self.sni = out.get("default_sni", "baidu.com")
 
     def request(self, flow: HTTPFlow):
         if flow.request.scheme == "http":
@@ -44,11 +56,19 @@ class SNIProxy:
     def tls_start_server(self, data: tls.TlsData):
         if isinstance(data.conn, connection.Server):
             (domain, _) = data.conn.address
+            # print("XXXXX",self.domains,self.sni)
+            # print("domain:", domain)
             if matches_any(self.domains, domain):
+                print("Matched one ordinary website: ", domain)
                 data.context.client.sni = self.sni
+                print("sni:", data.context.client.sni)
             elif matches_any(self.delicate_domains, domain):
+                print("Matched one delicate website: ", domain)
                 data.context.client.sni = rf"{domain}."
-
+            elif matches_any(self.googles, domain):
+                data.context.client.sni = "g.cn"
+                print("Matched one google website: ", domain, "sni changed to ", data.context.client.sni)
+                # print("sni:", data.context.client.sni)
         self.config.tls_start_server(data)
 
 
